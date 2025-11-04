e49e19bd0ecaa073ef7361c439572c7de0e1ca1e
Dxppi <Dxppi@vk.com>
2025-11-04 18:01:59 +0300

    added comment for messenger

--- domain/messenger.py
+++ domain/messenger.py
@@ -1,9 +1,12 @@
+"""Доменный интерфейс мессенджера"""
 from abc import ABC, abstractmethod
 
 
 class Messenger(ABC):
     @abstractmethod
-    def send_message(self, chat_id, text, **kwargs) -> None: ...
+    def send_message(self, chat_id, text, **kwargs) -> None:
+        """Отправляет текстовое сообщение пользователю."""
 
     @abstractmethod
-    def get_updates(self, **kwargs) -> list: ...
+    def get_updates(self, **kwargs) -> list:
+        """Возвращает новые апдейты (сообщения, события)."""
